573db354808579108a5bb7fdc8b376c73743ebac
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-11-30 16:30:12 +0100

    malo listener

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,9 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -8,11 +11,28 @@ public class Controller {
 
     public void loginClick(ActionEvent actionEvent) {
         System.out.println("Korisnicko ime: " + username.getText());
-        if (!username.getText().equals("elvir")) {
-            username.getStyleClass().add("neispravnoPolje");
-        }
-        else {
-            username.getStyleClass().add("ispravnoPolje");
-        }
+    }
+
+
+    @FXML
+    public void initialize() {
+        //username.getStyleClass().add("neispravnoPolje");
+        username.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                //System.out.printf("%s, %s\n", s, t1);
+                username.getStyleClass().removeAll("neispravnoPolje");
+                username.getStyleClass().removeAll("ispravnoPolje");
+                if (t1.equals("elvir")) {
+                    username.getStyleClass().add("ispravnoPolje");
+                }
+                else if (t1.isEmpty()) {
+                    username.getStyleClass().removeAll("neispravnoPolje");
+                }
+                else {
+                    username.getStyleClass().add("neispravnoPolje");
+                }
+            }
+        });
     }
 }
