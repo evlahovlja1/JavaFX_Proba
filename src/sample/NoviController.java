ff697dc28159bb7c678b5141553d22083c1eed23
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-11-30 17:10:17 +0100

    button za zatvaranje stage

--- src/sample/NoviController.java
+++ src/sample/NoviController.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class NoviController {
+    public void zatvoriProzorPropuhJe(ActionEvent actionEvent) {
+        Node n = (Node) actionEvent.getSource();
+        Stage stage = (Stage) n.getScene().getWindow();
+        stage.close();
+    }
 }
