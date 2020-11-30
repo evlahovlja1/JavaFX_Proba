b7bd7328880561124067bf207e2dee0117b239a7
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-11-30 16:57:00 +0100

    bidirectional property binding

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,14 @@
 package sample;
 
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    public TextField username;
+    public TextField loginField;
     private SimpleStringProperty login;
+
 
     public Controller() {
         login = new SimpleStringProperty("Login");
@@ -28,7 +27,7 @@ public class Controller {
     }
 
     public void loginClick(ActionEvent actionEvent) {
-        System.out.println("Korisnicko ime: " + username.getText());
+        System.out.println("Korisnicko ime: " + loginField.getText());
         System.out.println("login glasi: " + login.get());
         login.set("default");
     }
@@ -37,20 +36,22 @@ public class Controller {
     @FXML
     public void initialize() {
         //username.getStyleClass().add("neispravnoPolje");
-        username.textProperty().addListener((observableValue, s, t1) -> {
+        loginField.textProperty().addListener((observableValue, s, t1) -> {
                 //System.out.printf("%s, %s\n", s, t1);
-                username.getStyleClass().removeAll("neispravnoPolje");
-                username.getStyleClass().removeAll("ispravnoPolje");
+                loginField.getStyleClass().removeAll("neispravnoPolje");
+                loginField.getStyleClass().removeAll("ispravnoPolje");
                 if (t1.equals("elvir")) {
-                    username.getStyleClass().add("ispravnoPolje");
+                    loginField.getStyleClass().add("ispravnoPolje");
                 }
                 else if (t1.isEmpty()) {
-                    username.getStyleClass().removeAll("neispravnoPolje");
+                    loginField.getStyleClass().removeAll("neispravnoPolje");
                 }
                 else {
-                    username.getStyleClass().add("neispravnoPolje");
+                    loginField.getStyleClass().add("neispravnoPolje");
                 }
             }
         );
+
+        loginField.textProperty().bindBidirectional(login);
     }
 }
